5ad829739935ecd2cc637798e19afe6ce7de1df5
ALEXANDR <tanana1994@list.ru>
2021-10-26 17:31:06 +0300

    Сделал сборку

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,4 @@
 const gulp = require("gulp");
-const styles = require("./gulp/styles");
-const sync = require("browser-sync").create();
-const PATH = require("./gulp/_const");
-
-// Server
-
-const server = (done) => {
-  sync.init({
-    server: {
-      baseDir: "source",
-    },
-    cors: true,
-    notify: false,
-    ui: false,
-  });
-  done();
-};
-
-exports.server = server;
-
-// Watcher
-
-async function syncStyles() {
-  gulp.src(PATH.style.dest).pipe(sync.stream());
-}
-
-const watcher = () => {
-  gulp.watch(PATH.style.all, gulp.series(styles, syncStyles));
-  gulp.watch("source/*.html").on("change", sync.reload);
-};
-
-exports.build = gulp.series(styles);
-exports.default = gulp.series(styles, server, watcher);
-
-
-
-// ------------------------------------------------
-const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
